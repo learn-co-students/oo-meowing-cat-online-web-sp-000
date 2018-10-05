335192832590a5ec5942855cae8bc20be9448cf2
Austin  Burke <aburk3@gmail.com>
2018-10-05 00:54:47 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,34 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  def name(name_of_cat)
+    attr_accessor :name
+    puts "#{name_of_cat}.capitalize"
+
+  end
+
+  def meow
+    attr_accessor :meow
+    puts "meow!"
+  end
+end
+
+
+# class Cat
+#
+#   def name=(name)
+#     @name = name
+#   end
+#
+#   def name
+#     puts "#{name}.capitalize"
+#   end
+#
+#   def name=(meow)
+#     @meow = meow
+#   end
+#
+#   def meow
+#     puts "meow!"
+#   end
+#
+# end
